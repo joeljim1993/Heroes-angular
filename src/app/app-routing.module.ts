cb172edabc05fd4fae2772a5382b612ac48b29dd
joel jimenez <joel_jimenez@live.com>
2023-11-10 17:03:51 -0400

    rutas y rutas hijas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './auth/pages/layout-page/layout-page.component';
+import { Error404PageComponent } from './heroes/pages/error404-page/error404-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    component: LayoutPageComponent
+  },
+  {
+    path:"auth",
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule )
+  },
+  {
+    path:"heroes",
+    loadChildren: ()=> import('./heroes/heroes.module').then(m => m.HeroesModule)
+  },
+  {
+    path:"404",
+    component: Error404PageComponent
+  },
+  {
+    path:"",
+    redirectTo:"heroes",
+    pathMatch:'full'
+  },
+  {
+    path:"**",
+    redirectTo:"heroes",
+    pathMatch:'full'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
